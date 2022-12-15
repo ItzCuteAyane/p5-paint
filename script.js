0033c5c16db5ef217e2fd448810cdbd3c2d24a5d
ItzCuteAyane <118621219+ItzCuteAyane@users.noreply.github.com>
2022-12-15 14:34:41 +0700

    making it way more worse

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+let rectWidth;
+
 function setup() {
+    background(230);
     createCanvas(720, 480);
     noStroke();
+    rectWidth = width / 4;
 }
   
 function draw() {
@@ -9,6 +13,24 @@ function draw() {
       } else {
         fill(170, 233, 255);
       }
-    ellipse(mouseX,mouseY,40,40);
 }
 
+function keyPressed() {
+  let keyIndex = -1;
+  if (key >= 'a' && key <= 'z') {
+    keyIndex = key.charCodeAt(0) - 'a'.charCodeAt(0);
+  }
+  if (keyIndex === -1) {
+    // If it's not a letter key, clear the screen
+    background(230);
+  } else {
+    // change ellipse color
+    randFill_r = Math.floor(Math.random() * 255 + 1);
+    randFill_g = Math.floor(Math.random() * 255 + 1);
+    randFill_b = Math.floor(Math.random() * 255 + 1);
+    fill(randFill_r, randFill_g, randFill_b);
+    let x = map(keyIndex, 0, 25, 0, width - rectWidth);
+    ellipse(mouseX,mouseY,40,40);
+  }
+}
+ss
